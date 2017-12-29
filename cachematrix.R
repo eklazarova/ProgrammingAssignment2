d054c08151dd0d531026a0aa294bc07a1340fb71
eklazarova <ekaterina.lazarova@fao.org>
2017-12-29 15:03:00 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    s <- NULL
+    set <- function(y) {
+        x <<- y
+        s <<- NULL
+    }
+    get <- function() x
+    setcache <- function(solve) s <<- solve ()
+    getcache <- function() s
+    list(set = set, get = get,
+         setcache = setcache,
+         getcache = getcache)
 }
 
 
@@ -12,4 +22,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    s <- x$getsolve()
+    if(!is.null(s)) {
+        message("getting cached data")
+        return(s)
+    }
+    data <- x$get()
+    s <- solve(data, ...)
+    x$setsolve(s)
+    s
+}
 }
